10f362c950f1ac1896199137322dc2838b6fee07
saimon2828 <mdsaimon28285@gmail.com>
2026-05-19 11:37:08 +0600

    form

--- src/Pages/SendParcel/SendParcel.jsx
+++ src/Pages/SendParcel/SendParcel.jsx
@@ -3,32 +3,88 @@
 import { useForm } from "react-hook-form";
 
 const SendParcel = () => {
-    const {register, handleSubmit, formState:{errors}} = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm();
 
-    const handleSendParcel = data =>{
-
+    const handleSendParcel = data => {
+        console.log(data);
     }
     return (
         <div>
             <h2 className="text-5xl font-bold">Send a Parcel</h2>
-            <form onSubmit={handleSubmit(handleSendParcel)}>
-                {/* document */}
-                <div></div>
+            <form onSubmit={handleSubmit(handleSendParcel)} className="mt-12 p-4 text-black">
+                {/* parcel type */}
+                <div>
+                    <label className="label mr-4">
+                        <input type="radio" {...register('parcelType')} value="document" className="radio" defaultChecked />
+                        Document
+
+                    </label>
+                    <label className="label">
+                        <input type="radio" {...register('parcelType')} value="non-document" className="radio" />
+                        Non-document
+
+                    </label>
+                </div>
 
                 {/* parcel info: name, weight */}
-                <div></div>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-12 my-8">
+                    <fieldset className="fieldset">
+                        <label className="label">Parcel Name</label>
+                        <input type="text" {...register('parcelName')} className="input w-full" placeholder="Parcel Name" />
+                    </fieldset>
+                    <fieldset className="fieldset">
+                        <label className="label">Parcel Weight (kg)</label>
+                        <input type="number" {...register('parcelWeight')} className="input w-full" placeholder="Parcel Weight" />
+                    </fieldset>
+                </div>
 
                 {/* trow column */}
-                <div>
-                    {/* sender info */}
-                    <div>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+                    {/* sender details */}
+                  
+                    <fieldset className="fieldset">
+                          <h4 className="text-2xl font-semibold">Sender Details</h4>
+                        {/* sender name */}
+                        <label className="label">Sender Name</label>
+                        <input type="text" {...register('senderName')} className="input w-full" placeholder="Sender Name" />
+                        {/* sender address */}
+                        <label className="label mt-4">Sender Address</label>
+                        <input type="text" {...register('senderAddress')} className="input w-full" placeholder="Sender Address" />
+                        {/* sender phoneNumber */}
+                        <label className="label mt-4">Sender Number</label>
+                        <input type="number" {...register('senderPhoneNumber')} className="input w-full" placeholder="Sender Phone Number" />
+                        {/* sender District */}
+                        <label className="label mt-4">Sender District</label>
+                        <input type="text" {...register('senderDistrict')} className="input w-full" placeholder="Sender District" />
+                        {/* pickup instruction */}
+                        <label className="label mt-4">Pickup Instruction</label>
+                        <input type="text" {...register('pickupInstruction')} className="input w-full" placeholder="Pickup Instruction" />
+                    </fieldset>
+                      {/* receiver Details */}
+                 <fieldset className="fieldset">
+                      <h4 className="text-2xl font-semibold">Receiver Details
+                      </h4>
 
-                    </div>
-                    {/* receiver infi */}
-                    <div>
-
-                    </div>
+                        {/* sender name */}
+                        <label className="label">Sender Name</label>
+                        <input type="text" {...register('senderName')} className="input w-full" placeholder="Sender Name" />
+                        {/* sender address */}
+                        <label className="label mt-4">Sender Address</label>
+                        <input type="text" {...register('senderAddress')} className="input w-full" placeholder="Sender Address" />
+                        {/* sender phoneNumber */}
+                        <label className="label mt-4">Sender Number</label>
+                        <input type="number" {...register('senderPhoneNumber')} className="input w-full" placeholder="Sender Phone Number" />
+                        {/* sender District */}
+                        <label className="label mt-4">Sender District</label>
+                        <input type="text" {...register('senderDistrict')} className="input w-full" placeholder="Sender District" />
+                        {/* pickup instruction */}
+                        <label className="label mt-4">Pickup Instruction</label>
+                        <input type="text" {...register('pickupInstruction')} className="input w-full" placeholder="Pickup Instruction" />
+                    </fieldset>
                 </div>
+              
+
+                <input type="submit" value="Send parcel" className="btn btn-primary text-black" />
             </form>
         </div>
     );
